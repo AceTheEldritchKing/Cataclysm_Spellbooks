c958b0a15a5dbb427ef5175b937ecffe2a41d6e0
Ace The King <solcat0414@gmail.com>
2024-08-25 22:23:26 -0400

    Cleaned up Ignis Armor renderer code, made fire block rendering while wearing Ignitium Wizard helmet cancel (no more fire!!)

--- src/main/java/net/acetheeldritchking/cataclysm_spellbooks/entity/render/IgnisWizardArmorRenderer.java
+++ src/main/java/net/acetheeldritchking/cataclysm_spellbooks/entity/render/IgnisWizardArmorRenderer.java
@@ -6,7 +6,6 @@ import mod.azure.azurelib.renderer.layer.AutoGlowingGeoLayer;
 import net.acetheeldritchking.cataclysm_spellbooks.entity.armor.IgnisWizardArmorModel;
 import net.acetheeldritchking.cataclysm_spellbooks.items.armor.IgnisWizardArmorItem;
 import net.minecraft.world.entity.EquipmentSlot;
-import org.jetbrains.annotations.Nullable;
 
 public class IgnisWizardArmorRenderer extends GeoArmorRenderer<IgnisWizardArmorItem> {
     public IgnisWizardArmorRenderer()
@@ -15,89 +14,42 @@ public class IgnisWizardArmorRenderer extends GeoArmorRenderer<IgnisWizardArmorI
         addRenderLayer(new AutoGlowingGeoLayer<>(IgnisWizardArmorRenderer.this));
     }
 
-    // Do I look like I know what I'm doing? No <3
-    String head = "armorHead";
-    String body = "armorBody";
-    String rightArm = "armorRightArm";
-    String leftArm = "armorLeftArm";
-    String rightLeg = "armorRightLeg";
-    String leftLeg = "armorLeftLeg";
     String armorLeggingTorsoLayer = "armorLeggingTorsoLayer";
-    String righBoot = "armorRightBoot";
-    String leftBoot = "armorLeftBoot";
 
-    @Override
-    protected void applyBoneVisibilityBySlot(EquipmentSlot currentSlot) {
-        this.setAllVisible(false);
-        switch (currentSlot) {
-            case HEAD -> this.setBoneVisible(this.getHeadBone(), true);
-            case CHEST -> {
-                this.setBoneVisible(this.getBodyBone(), true);
-                this.setBoneVisible(this.getRightArmBone(), true);
-                this.setBoneVisible(this.getLeftArmBone(), true);
-            }
-            case LEGS -> {
-                this.setBoneVisible(this.armorLeggingTorsoBone(), true);
-                this.setBoneVisible(this.getRightLegBone(), true);
-                this.setBoneVisible(this.getLeftLegBone(), true);
-            }
-            case FEET -> {
-                this.setBoneVisible(this.getRightBootBone(), true);
-                this.setBoneVisible(this.getLeftBootBone(), true);
-            }
-        }
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getHeadBone() {
-        return model.getBone(head).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getBodyBone() {
-        return model.getBone(body).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getLeftArmBone() {
-        return model.getBone(leftArm).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getRightArmBone() {
-        return model.getBone(rightArm).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getLeftLegBone() {
-        return model.getBone(leftLeg).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getRightLegBone() {
-        return model.getBone(rightLeg).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getLeftBootBone() {
-        return model.getBone(leftBoot).orElse(null);
-    }
-
-    @Nullable
-    @Override
-    public GeoBone getRightBootBone() {
-        return model.getBone(righBoot).orElse(null);
-    }
-
+    // New bone
     private GeoBone armorLeggingTorsoBone()
     {
         return new GeoBone(this.getBodyBone(), armorLeggingTorsoLayer, true, null, true, true);
+    }
+
+    @Override
+    protected void applyBoneVisibilityBySlot(EquipmentSlot currentSlot) {
+        this.setBoneVisible(getHeadBone(),false);
+        this.setBoneVisible(getBodyBone(),false);
+        this.setBoneVisible(getRightArmBone(),false);
+        this.setBoneVisible(getLeftArmBone(),false);
+        this.setBoneVisible(getRightLegBone(),false);
+        this.setBoneVisible(getLeftLegBone(),false);
+        this.setBoneVisible(getRightBootBone(),false);
+        this.setBoneVisible(getLeftBootBone(),false);
+        this.setBoneVisible(armorLeggingTorsoBone(),false);
+
+        switch (currentSlot) {
+            case HEAD -> this.setBoneVisible(getHeadBone(), true);
+            case CHEST -> {
+                this.setBoneVisible(getBodyBone(), true);
+                this.setBoneVisible(getRightArmBone(), true);
+                this.setBoneVisible(getLeftArmBone(), true);
+            }
+            case LEGS -> {
+                this.setBoneVisible(getRightLegBone(), true);
+                this.setBoneVisible(getLeftLegBone(), true);
+                this.setBoneVisible(armorLeggingTorsoBone(), true);
+            }
+            case FEET -> {
+                this.setBoneVisible(getRightBootBone(), true);
+                this.setBoneVisible(getLeftBootBone(), true);
+            }
+        }
     }
 }
--- src/main/java/net/acetheeldritchking/cataclysm_spellbooks/events/ClientEvents.java
+++ src/main/java/net/acetheeldritchking/cataclysm_spellbooks/events/ClientEvents.java
@@ -1,13 +1,45 @@
 package net.acetheeldritchking.cataclysm_spellbooks.events;
 
 import net.acetheeldritchking.cataclysm_spellbooks.CataclysmSpellbooks;
+import net.acetheeldritchking.cataclysm_spellbooks.registries.ItemRegistries;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.material.FogType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderBlockScreenEffectEvent;
+import net.minecraftforge.client.event.ViewportEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = CataclysmSpellbooks.MOD_ID, value = Dist.CLIENT)
 public class ClientEvents {
 
-    // Client Side
+    @SubscribeEvent
+    public void onFogDensity(ViewportEvent.RenderFog event)
+    {
+        // Lava vision for Ignis helmet
+        FogType fogType = event.getCamera().getFluidInCamera();
+        ItemStack itemStack = Minecraft.getInstance().player.getInventory().getArmor(3);
+        if (itemStack.is(ItemRegistries.IGNITIUM_WIZARD_HELMET.get()) && fogType == FogType.LAVA)
+        {
+            event.setNearPlaneDistance(0);
+            event.setFarPlaneDistance(event.getRenderer().getRenderDistance() * 0.5f);
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void renderBlockScreenEvent(RenderBlockScreenEffectEvent event)
+    {
+        ItemStack itemStack = Minecraft.getInstance().player.getInventory().getArmor(3);
+        if (itemStack.is(ItemRegistries.IGNITIUM_WIZARD_HELMET.get()) && Minecraft.getInstance().player.isInLava())
+        {
+            if (event.getOverlayType() == RenderBlockScreenEffectEvent.OverlayType.FIRE)
+            {
+                event.setCanceled(true);
+            }
+        }
+    }
 }
